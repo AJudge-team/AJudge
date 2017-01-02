559d946042957a7da5667219445e17fa756065e0
SeungJu Choi <csjaj9772@gmail.com>
2017-01-02 22:00:05 +0900

    add comment and edit method, parameter name
    - change parameter 'data' into 'source'
    - tarfile.Tarfile -> tarfile.open
    - add explanation step by step

--- sandbox/sandbox.py
+++ sandbox/sandbox.py
@@ -33,9 +33,8 @@ class Sandbox(object):
         self.docker_client.remove_container(container=self.container.get('Id'))
         self.docker_client.close()
 
-
-# execute cmd in container
     def exec(self, cmd: str) -> dict:
+        # execute cmd in container
         wrapped_cmd = "bash -c \"" + cmd + "\""
         exec_id = self.docker_client.exec_create(self.container.get('Id'),
                                                  wrapped_cmd,
@@ -48,17 +47,17 @@ class Sandbox(object):
         cur_directory = os.path.dirname(__file__)  # find absolute path
         os.chdir(cur_directory)
 
-        data_tar_stream = BytesIO()
-        data_tar = tarfile.TarFile(fileobj=data_tar_stream, mode="w")
+        data_tar_stream = BytesIO()  # in memory binary stream
+        data_tar = tarfile.open(fileobj=data_tar_stream, mode="w")
         data = open(host_directory).read().encode("utf8")  # convert str to bytes
 
         tarinfo = tarfile.TarInfo(name="server.py")
-        tarinfo.size = len(data)
-        tarinfo.mtime = time.time()
+        tarinfo.size = len(data)  # size in bytes
 
-        data_tar.addfile(tarinfo, BytesIO(data))
+        data_tar.addfile(tarinfo, BytesIO(data))  # generate file with tarinfo, binary stream data
         data_tar.close()
-        data_tar_stream.seek(0)
+
+        data_tar_stream.seek(0)  # set stream position is start of the stream
 
         return {"Success": self.docker_client.put_archive(
             container=self.container.get('Id'),
@@ -66,18 +65,18 @@ class Sandbox(object):
             data=data_tar_stream
         ), "Bytes": data}
 
-    def copy_bytes_to_container(self, bytes_data: bytes, container_directory: str, file_name: str) -> dict:
-        data_tar_stream = BytesIO()
-        data_tar = tarfile.TarFile(fileobj=data_tar_stream, mode="w")
-        data = bytes_data
+    def copy_bytes_to_container(self, source: bytes, container_directory: str, container_file_name: str) -> dict:
+        data_tar_stream = BytesIO()  # in memory binary stream
+        data_tar = tarfile.open(fileobj=data_tar_stream, mode="w")
+        data = source
 
-        tarinfo = tarfile.TarInfo(name=file_name)
-        tarinfo.size = len(data)
-        tarinfo.mtime = time.time()
+        tarinfo = tarfile.TarInfo(name=container_file_name)
+        tarinfo.size = len(data)  # size in bytes
 
-        data_tar.addfile(tarinfo, BytesIO(data))
+        data_tar.addfile(tarinfo, BytesIO(data))  # generate file with tarinfo, binary stream data
         data_tar.close()
-        data_tar_stream.seek(0)
+
+        data_tar_stream.seek(0)  # set stream position is start of the stream
 
         return {"Success": self.docker_client.put_archive(
             container=self.container.get('Id'),
